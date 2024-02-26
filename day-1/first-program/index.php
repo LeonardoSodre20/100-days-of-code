263bb041ad3dfcbc85b6c18e9ec5e503dc69ea6f
Leonardo Vieira Sodré <LeonardoSodre25@outlook.com>
2024-02-26 20:15:51 -0300

    :sparkles: Feat: Day-1
    Today learned concepts of language PHP and its peculiarities and too and some of its syntax

---
+++ day-1/first-program/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Program in PHP</title>
+</head>
+
+<body>
+    <?php
+    echo '<h1>Leonardo Vieira Sodré</h1>'
+    ?>
+
+    <h1>
+        <?php
+        echo "Hello World \u{1F30E}"
+        ?>
+    </h1>
+
+    <h1>
+        <?php
+        print "Hello World with print function"
+        ?>
+    </h1>
+</body>
+
+</html>
---
+++ day-1/variables/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables in PHP</title>
+
+    <style>
+    p {
+        color: #f10000;
+        font-size: 2em;
+    }
+    </style>
+</head>
+
+<body>
+    <p>
+        <?php
+        $name = 'Leonardo Vieira Sodré';
+        echo "Meu nome é $name"
+        ?>
+    </p>
+</body>
+
+</html>
